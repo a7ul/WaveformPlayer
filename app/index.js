330512a8936fef362e1aceba860dda476310f59a
Atul <atulanand94@gmail.com>
2017-05-11 19:00:25 +0700

    Added eslintrc and working web pack

--- app/index.js
+++ app/index.js
@@ -1,12 +1,5 @@
-import _ from 'lodash';
+console.log('TEST32');
 
-function component () {
-  var element = document.createElement('div');
+module.exports = {
 
-  /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+};
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var path = require('path');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  target: 'electron'
+};
