bccf4eb4bbb7f8fe1fdd2f736c479c49844e3791
Atul <atulanand94@gmail.com>
2017-05-15 00:46:55 +0700

    small formatting

--- app/utils/process.util.js
+++ app/utils/process.util.js
@@ -5,19 +5,21 @@ import noop from 'lodash/noop';
 // getProcessExecutor('./test.sh').execute('--param1','abc');  will execute ./test.sh --param1 abc
 // getProcessExecutor('./test.sh').binaryPath;  will give ./test.sh
 
-export const getProcessExecutor = (binaryPath = null, onDataHandler = noop) => ({
-  binaryPath,
-  execute: (...commands) => {
-    const promisified = new Promise((resolve, reject) => {
-      console.log('command:', binaryPath, commands.join(' '));
-      const process = childProcess.execFile(binaryPath, commands, (error, stdout) => {
-        if (error) {
-          return reject(error);
-        }
-        resolve(stdout);
+export const getProcessExecutor = (binaryPath = null, onDataHandler = noop) => (
+  {
+    binaryPath,
+    execute: (...commands) => {
+      const promisified = new Promise((resolve, reject) => {
+        console.log('command:', binaryPath, commands.join(' '));
+        const process = childProcess.execFile(binaryPath, commands, (error, stdout) => {
+          if (error) {
+            return reject(error);
+          }
+          resolve(stdout);
+        });
+        process.stdout.on('data', onDataHandler);
       });
-      process.stdout.on('data', onDataHandler);
-    });
-    return promisified;
+      return promisified;
+    }
   }
-});
+);
