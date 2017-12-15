77a473f7211e31d8fafc23e10a87a44bf76df27d
Atul R <atulanand94@gmail.com>
2017-12-16 01:52:00 +0530

    Added clean code stuff

---
+++ scripts/installDevtools.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+
+const devToolsInstaller = require('electron-devtools-installer');
+
+const {REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS} = devToolsInstaller;
+const installExtension = devToolsInstaller.default;
+
+module.exports = ()=> {
+  return installExtension(REACT_DEVELOPER_TOOLS)
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log('An error occurred: ', err))
+    .then(()=>installExtension(REDUX_DEVTOOLS))
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log('An error occurred: ', err));
+};
