43e25d09f3d82a1f2657bd1a11428cd335e73396
Atul R <atulanand94@gmail.com>
2017-12-16 17:05:43 +0530

    change to material icons

--- app/features/Controller/index.js
+++ app/features/Controller/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import styles from './style.css';
+import Icon from '../Shared/Icon';
 
 class Controller extends React.Component {
   render () {
     return (
       <div className={styles.controller}>
-        <div>Play</div>
-        <div>Pause</div>
-        <div>Prev</div>
-        <div>Next</div>
+        <div><Icon name="play_arrow" /></div>
+        <div><Icon name="pause" /></div>
+        <div><Icon name="skip_previous" /></div>
+        <div><Icon name="skip_next" /></div>
       </div>
     );
   }
