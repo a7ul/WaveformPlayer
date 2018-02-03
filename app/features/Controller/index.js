1ae8676ab0d310049d8096a38984428d87e31f08
Atul R <atulanand94@gmail.com>
2018-02-03 15:19:05 +0530

    bring back demo code

--- app/features/Controller/index.js
+++ app/features/Controller/index.js
@@ -4,33 +4,33 @@ import PropTypes from 'prop-types';
 import * as styles from './style';
 import PrimaryControls from './components/PrimaryControls';
 import * as actions from './redux';
-// import {decodeAudio, MusicSource} from '../../utils/audio';
-// import {readFileToArrayBuffer} from '../../utils/common';
+import { decodeAudio, MusicSource } from '../../utils/audio';
+import { readFileToArrayBuffer } from '../../utils/common';
 
-// let mSource = null;
-// readFileToArrayBuffer('/Users/atulr/Music/iTunes/iTunes
-// Media/Music/Evanescence/Unknown Album/Evanescence - Bring Me To Life.mp3')
-//   .then(decodeAudio)
-//   .then((audioBuffer) => {
-//     mSource = new MusicSource(audioBuffer);
-//     mSource.setOnTickHandler((d) => console.log('TICK', d));
-//     console.log('Alldone');
-//   });
-// .then(createAudioSourceNode).then(console.log).catch(console.log);
+let mSource = null;
+readFileToArrayBuffer('/Users/atulr/Desktop/AlanWalker_Faded.mp3')
+  .then(decodeAudio)
+  .then((audioBuffer) => {
+    mSource = new MusicSource(audioBuffer);
+    mSource.setOnTickHandler((d) => console.log('TICK', d));
+    console.log('Alldone');
+  })
+  .then(console.log)
+  .catch(console.log);
 
 class Controller extends React.Component {
   togglePlay = () => {
     const { setPlayStatus, isPlaying } = this.props;
     setPlayStatus(!isPlaying);
-    // if (mSource) {
-    //   if (!isPlaying) {
-    //     mSource.play();
-    //     console.log('PLAYING');
-    //   } else {
-    //     mSource.pause();
-    //     console.log('PAUSE');
-    //   }
-    // }
+    if (mSource) {
+      if (!isPlaying) {
+        mSource.play();
+        console.log('PLAYING');
+      } else {
+        mSource.pause();
+        console.log('PAUSE');
+      }
+    }
   }
 
   render() {
@@ -62,12 +62,12 @@ Controller.propTypes = {
   onPrev: PropTypes.func
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   isPlaying: state.controller.isPlaying
 });
 
-const mapDispatchToProps = dispatch => ({
-  setPlayStatus: status => dispatch(actions.setPlayStatus(status)),
+const mapDispatchToProps = (dispatch) => ({
+  setPlayStatus: (status) => dispatch(actions.setPlayStatus(status)),
   onNext: () => {
     // mSource.seek(mSource.playbackTime + 3);
   },
